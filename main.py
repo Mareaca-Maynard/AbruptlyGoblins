d5f47dfede2964e422fc9dd9add536e702283d82
Mareaca Maynard <136956298+Mareaca-Maynard@users.noreply.github.com>
2023-06-26 21:32:40 -0500

    first gamer

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+gamers = []
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def add_gamer(gamer, gamers_list):
+    if gamer.get("name") and gamer.get("availability"):
+        gamers_list.append(gamer)
+    else:
+        print("Gamer missing critical information")
 
+kimberly = {
+    'name': "Kimberly Warner",
+    'availability': ["Monday", "Tuesday", "Friday"]
+}
+add_gamer(kimberly, gamers)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(gamers)
