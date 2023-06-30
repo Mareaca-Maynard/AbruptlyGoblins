bbd0bf76066cffe0c50e59a8b7c52693b56688c8
Mareaca Maynard <136956298+Mareaca-Maynard@users.noreply.github.com>
2023-06-29 20:00:49 -0500

    second night email

--- main.py
+++ main.py
@@ -77,4 +77,12 @@ def send_email(gamers_who_can_attend, day, game):
     for gamer in gamers_who_can_attend:
         print(form_email.format(name=gamer['name'], day_of_week=day, game=game))
 
-send_email(attending_game_night, game_night, 'Abruptly Goblins!')
+# send_email(attending_game_night, game_night, 'Abruptly Goblins!')
+
+unable_to_attend_best_night = [gamer for gamer in gamers if game_night not in gamer['availability']]
+second_night_availability = build_daily_frequency_table()
+calculate_availability(unable_to_attend_best_night, second_night_availability)
+second_night = find_best_night(second_night_availability)
+
+available_second_game_night = available_on_night(gamers, second_night)
+send_email(available_second_game_night, second_night, "Abruptly Goblins!")
